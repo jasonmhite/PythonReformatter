82bd7756495135678bd74840a89a7431029df62e
Jason Hite <jasonmhite@gmail.com>
2015-11-18 14:26:37 -0500

    Take initial lines too, because that's what vim passes apparently

--- PythonFormat.hs
+++ PythonFormat.hs
@@ -1,54 +1,95 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE MultiWayIf #-}
 module Main where
 
 import Control.Monad.State hiding (join)
 import Data.String.Utils -- MissingH
 import Data.List (filter)
 
-type ReaderState = (String, Int, Int, Int) -- out, (, [, {
+-- TODO : Need to read the initial indent and preserve that
+
+type ReaderState = (String, String, Int, Int, Int) -- out, close, (, [, {
 
 advanceHead :: String -> State ReaderState String
 advanceHead "" = do
-    (out, _, _, _) <- get
+    (out, _, _, _, _) <- get
     return out
     
+-- Effectively a state machine that reads characters off a tape, hence the name
 advanceHead (x:xs) = do
-    (out, cp, cb, cc) <- get
-    case x of
-        '(' -> put (out ++ [x], cp + 1, cb, cc)
-        '[' -> put (out ++ [x], cp, cb + 1, cc)
-        '{' -> put (out ++ [x], cp, cb, cc + 1)
-        ')' -> put (out ++ [x], cp - 1, cb, cc)
-        ']' -> put (out ++ [x], cp, cb - 1, cc)
-        '}' -> put (out ++ [x], cp, cb, cc - 1)
-        ',' -> if cp == 0 && cb == 0 && cc == 0
-                 then put (out ++ [x, '\n'], cp, cb, cc)
-                 else put (out ++ [x], cp, cb, cc)
-        otherwise -> put (out ++ [x], cp, cb, cc)       
-    advanceHead xs
+    (out, cl, cp, cb, cc) <- get
+    if | cl == "" -> case x of
+            '(' -> put (out ++ [x] ++ "\n", ")", cp, cb, cc)
+            '[' -> put (out ++ [x] ++ "\n", "]", cp, cb, cc)
+            '{' -> put (out ++ [x] ++ "\n", "}", cp, cb, cc)
+            otherwise -> put (out ++ [x], cl, cp, cb, cc)
+       | x == ')' && cl == ")" && cp == 0 ->
+            put (out ++ "\n" ++ [x], cl, cp - 1, cb, cc)
+       | x == ']' && cl == "]" && cb == 0 ->
+            put (out ++ "\n" ++ [x], cl, cp, cb - 1, cc) 
+       | x == '}' && cl == "}" && cc == 0 ->
+            put (out ++ "\n" ++ [x], cl, cp, cb, cc - 1)  
+       | otherwise -> case x of
+            '(' -> put (out ++ [x], cl, cp + 1, cb, cc)
+            '[' -> put (out ++ [x], cl, cp, cb + 1, cc)
+            '{' -> put (out ++ [x], cl, cp, cb, cc + 1)
+            ')' -> put (out ++ [x], cl, cp - 1, cb, cc)
+            ']' -> put (out ++ [x], cl, cp, cb - 1, cc)
+            '}' -> put (out ++ [x], cl, cp, cb, cc - 1)
+            ',' -> if cp == 0 && cb == 0 && cc == 0
+                     then put (out ++ [x, '\n'], cl, cp, cb, cc)
+                     else put (out ++ [x], cl, cp, cb, cc)
+            otherwise -> put (out ++ [x], cl, cp, cb, cc)       
+    advanceHead xs 
 
 startState :: ReaderState
-startState = ("", 0, 0, 0)
+startState = ("", "", 0, 0, 0)
 
 breakLines :: String -> String
 breakLines s = evalState (advanceHead s) startState
 
 addTerminalComma :: [String] -> [String]
-addTerminalComma s = let l = last s
-                         c = last l :: Char
-                         d = case c of
-                               ',' -> ""
-                               otherwise -> ","
-                     in init s ++ [l ++ d]
+addTerminalComma [] = []
+addTerminalComma [x] = [x]
+addTerminalComma [x, y] = [x, y]
+addTerminalComma x = let fline = head x
+                         lline = last x
+                         body = (init . tail) x
+                         insBody = init body
+                         endBody = (rstrip . last) body
+                         newBody = if (not . endswith ",") endBody
+                                     then endBody ++ ","
+                                     else endBody
+                     in [fline]
+                          ++ insBody
+                          ++ [newBody]
+                          ++ [lline]
+
+indentChar :: String
+indentChar = "    "
+
+-- The second and third patterns are probably not needed
+indentLines :: [String] -> [String]
+indentLines [] = []
+indentLines [x] = [x]
+indentLines [x, y] = [x, y]
+indentLines x  = let fline = head x
+                     lline = last x
+                     ins = (init . tail) x 
+                 in [fline]
+                      ++ map (indentChar ++) ins
+                      ++ [lline]
 
 formatString :: String -> String
 formatString = unlines
                 . addTerminalComma
+                . indentLines
                 . filter (/= "")
                 . map strip
                 . lines
                 . breakLines
 
+-- Currently a bit broken, at least as a vim filter, hence disabled
 removeTerminalComma :: String -> String
 removeTerminalComma s = case (last s :: Char) of
                             ',' -> init s
@@ -63,7 +104,7 @@ joinString = removeTerminalComma
 
 joinOrSplit :: String -> String
 joinOrSplit s
-    | (length . lines) l > 1 = joinString l
+    {-| (length . lines) l > 1 = joinString l-}
     | otherwise = formatString l
     where l = rstrip s
 
