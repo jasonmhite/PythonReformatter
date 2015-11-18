9a649edac3726725336ea876ac52cb3a8e62363a
Jason Hite <jasonmhite@gmail.com>
2015-11-18 16:11:15 -0500

    Mostly working, will reformat to use state

--- PythonFormat.hs
+++ PythonFormat.hs
@@ -5,47 +5,50 @@ module Main where
 import Control.Monad.State hiding (join)
 import Data.String.Utils -- MissingH
 import Data.List (filter)
+import Data.Char (isSpace)
 
 -- TODO : Need to read the initial indent and preserve that
+-- TODO : Also need to preserve trailing characters after the close is found
 
-type ReaderState = (String, String, Int, Int, Int) -- out, close, (, [, {
+type ReaderState = (String, String, String, Int, Int, Int) -- out, close, indent, (, [, {
 
-advanceHead :: String -> State ReaderState String
+advanceHead :: String -> State ReaderState (String, String)
 advanceHead "" = do
-    (out, _, _, _, _) <- get
-    return out
+    (out, _, ind, _, _, _) <- get
+    return (out, ind)
     
 -- Effectively a state machine that reads characters off a tape, hence the name
 advanceHead (x:xs) = do
-    (out, cl, cp, cb, cc) <- get
-    if | cl == "" -> case x of
-            '(' -> put (out ++ [x] ++ "\n", ")", cp, cb, cc)
-            '[' -> put (out ++ [x] ++ "\n", "]", cp, cb, cc)
-            '{' -> put (out ++ [x] ++ "\n", "}", cp, cb, cc)
-            otherwise -> put (out ++ [x], cl, cp, cb, cc)
+    (out, cl, ind, cp, cb, cc) <- get
+    if | isSpace x && cl == "" && out == "" -> put (out, cl, ind ++ [x], cp, cb, cc)
+       | cl == "" -> case x of
+            '(' -> put (out ++ [x] ++ "\n", ")", ind, cp, cb, cc)
+            '[' -> put (out ++ [x] ++ "\n", "]", ind, cp, cb, cc)
+            '{' -> put (out ++ [x] ++ "\n", "}", ind, cp, cb, cc)
+            otherwise -> put (out ++ [x], cl, ind, cp, cb, cc)
        | x == ')' && cl == ")" && cp == 0 ->
-            put (out ++ "\n" ++ [x], cl, cp - 1, cb, cc)
+            put (out ++ "\n" ++ [x], cl, ind, cp - 1, cb, cc)
        | x == ']' && cl == "]" && cb == 0 ->
-            put (out ++ "\n" ++ [x], cl, cp, cb - 1, cc) 
+            put (out ++ "\n" ++ [x], cl, ind, cp, cb - 1, cc) 
        | x == '}' && cl == "}" && cc == 0 ->
-            put (out ++ "\n" ++ [x], cl, cp, cb, cc - 1)  
+            put (out ++ "\n" ++ [x], cl, ind, cp, cb, cc - 1)  
        | otherwise -> case x of
-            '(' -> put (out ++ [x], cl, cp + 1, cb, cc)
-            '[' -> put (out ++ [x], cl, cp, cb + 1, cc)
-            '{' -> put (out ++ [x], cl, cp, cb, cc + 1)
-            ')' -> put (out ++ [x], cl, cp - 1, cb, cc)
-            ']' -> put (out ++ [x], cl, cp, cb - 1, cc)
-            '}' -> put (out ++ [x], cl, cp, cb, cc - 1)
+            '(' -> put (out ++ [x], cl, ind, cp + 1, cb, cc)
+            '[' -> put (out ++ [x], cl, ind, cp, cb + 1, cc)
+            '{' -> put (out ++ [x], cl, ind, cp, cb, cc + 1)
+            ')' -> put (out ++ [x], cl, ind, cp - 1, cb, cc)
+            ']' -> put (out ++ [x], cl, ind, cp, cb - 1, cc)
+            '}' -> put (out ++ [x], cl, ind, cp, cb, cc - 1)
             ',' -> if cp == 0 && cb == 0 && cc == 0
-                     then put (out ++ [x, '\n'], cl, cp, cb, cc)
-                     else put (out ++ [x], cl, cp, cb, cc)
-            otherwise -> put (out ++ [x], cl, cp, cb, cc)       
+                     then put (out ++ [x, '\n'], cl, ind, cp, cb, cc)
+                     else put (out ++ [x], cl, ind, cp, cb, cc)
+            otherwise -> put (out ++ [x], cl, ind, cp, cb, cc)       
     advanceHead xs 
 
 startState :: ReaderState
-startState = ("", "", 0, 0, 0)
+startState = ("", "", "", 0, 0, 0)
 
-breakLines :: String -> String
+breakLines :: String -> (String, String)
 breakLines s = evalState (advanceHead s) startState
 
 addTerminalComma :: [String] -> [String]
@@ -80,13 +83,17 @@ indentLines x  = let fline = head x
                       ++ map (indentChar ++) ins
                       ++ [lline]
 
+z (x, y) = (lines x, y)
+
+-- TODO : Reformat to use a state monad, make the composes binds
 formatString :: String -> String
 formatString = unlines
                 . addTerminalComma
                 . indentLines
-                . filter (/= "")
-                . map strip
-                . lines
+                . (\(x, i) -> map (i ++) x)
+                . (\(x, i) -> (filter (/= "") x, i))
+                . (\(x, i) -> (map strip x, i))
+                . (\(x, i)  -> (lines x, i))
                 . breakLines
 
 -- Currently a bit broken, at least as a vim filter, hence disabled
